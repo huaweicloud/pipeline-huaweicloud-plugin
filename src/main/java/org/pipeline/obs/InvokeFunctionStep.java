63b4baf111386323e3a92579024c2c1a0c598fa5
zhongjun2 <jun.zhongjun@huawei.com>
2019-10-31 11:40:57 +0800

    Update InvokeFunctionStep.java

--- src/main/java/org/pipeline/obs/InvokeFunctionStep.java
+++ src/main/java/org/pipeline/obs/InvokeFunctionStep.java
@@ -2,9 +2,6 @@
  * -
  * #%L
  * Pipeline: OBS Steps
- * %%
- * Copyright (C) 2017 Taimos GmbH
- * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
